fb6e9d4e0df462c98ec48574ade503ff0479e8c5
Sebastian Bartos <seth.kriticos@gmail.com>
2012-10-31 16:06:47 +0100

    Refactoring to Qt Designer UI completed
    Does the same as before, but a lot of GUI clutter
    removed form python code.
    Furthermore the widget classes have been separated
    into individual files.

--- indextablectrl.py
+++ indextablectrl.py
@@ -9,11 +9,18 @@ This file contains the table model that displays information on the
 project index.
 """
 
+import time
+
 from PySide import QtCore
 from PySide import QtGui
 
 class IndexTableCtrl(QtCore.QObject):
     
+    """
+        Stores the data for the main listing and contains the 
+        interactions with the table.
+    """
+
     def __init__(self, table_view):
         
         super().__init__()
@@ -33,6 +40,91 @@ class IndexTableCtrl(QtCore.QObject):
         self.model.setHorizontalHeaderLabels(self.headers)
         self.data = {'lastid' : 0}
         self.view.setModel(self.model)
+
+    def reload_data(self, data):
+
+        self.data = data
+        self.model.clear()
+        self.model.setHorizontalHeaderLabels(self.headers)
+        
+        for key in self.data:
+            if isinstance(key, int):
+                self.model.appendRow([
+                    QtGui.QStandardItem(str(self.data[key][0])),
+                    QtGui.QStandardItem(str(self.data[key][1])),
+                    QtGui.QStandardItem(str(self.data[key][2])),
+                    QtGui.QStandardItem(str(self.data[key][3])),
+                    QtGui.QStandardItem(str(self.data[key][4])),
+                    QtGui.QStandardItem(str(self.data[key][5])),
+                    QtGui.QStandardItem(str(self.data[key][5])),
+                    QtGui.QStandardItem(str(self.data[key][7])),
+                    QtGui.QStandardItem(str(key))
+                ])
+
+    def removeActiveRow(self):
+    
+        rowindex = self.currentIndex().row()
+        child = self.model.index(rowindex, 8) # 8 = ID column
+        entryid = self.model.itemFromIndex(child).text()
+        del self.data[int(entryid)]
+        self.model.removeRow(rowindex)
+
+    def addNewRow(self, name, entrytype, category, priority, challange):
+
+        entryid = self.data['lastid'] + 1
+        entry = [
+            name, entrytype, 'Spark', category,
+            priority, challange, '0.0.0',
+            int(time.time())
+        ]
+
+        self.data[entryid] = entry
+        self.data['lastid'] += 1
+        
+        self.model.appendRow([
+            QtGui.QStandardItem(name),
+            QtGui.QStandardItem(entrytype),
+            QtGui.QStandardItem('Spark'),
+            QtGui.QStandardItem(category),
+            QtGui.QStandardItem(str(priority)),
+            QtGui.QStandardItem(str(challange)),
+            QtGui.QStandardItem('0.0.0'),
+            QtGui.QStandardItem(str(int(time.time()))),
+            QtGui.QStandardItem(str(entryid))
+        ])
+    
+    def changeRow(self, itemid, rowindex, name, entrytype, 
+                  category, priority, challange):
+
+        status = self.model.itemFromIndex(
+            self.model.index(rowindex, 2)).text()
+        
+        version = self.model.itemFromIndex(
+            self.model.index(rowindex, 6)).text()
+        
+        entry = [
+            name, entrytype, status, category,
+            priority, challange, version,
+            int(time.time())
+        ]
+
+        self.data[itemid] = entry
+
+        tabdat = (
+            (0, name), 
+            (1, entrytype),
+            (2, status),
+            (3, category),
+            (4, priority),
+            (5, challange),
+            (6, version),
+            (7, int(time.time())),
+            (8, itemid)
+        )
+
+        for index, value in tabdat:
+            self.model.setData(
+                self.model.index(rowindex, index), value)
 
 
 # vim: set ts=4 sw=4 ai si expandtab:
--- mainwindow.py
+++ mainwindow.py
@@ -8,10 +8,13 @@ Component:			Main window
 This file contains the MainWindow class for Nelia.
 """
 
+import os, pickle, time
+
 from PySide import QtCore
 from PySide import QtGui
 from PySide import QtUiTools
 from indextablectrl import IndexTableCtrl
+from newchangeindex import NewChangeIndex
 
 from pprint import pprint
 
@@ -20,16 +23,68 @@ class MainWindow(QtCore.QObject):
     def __init__(self):
         
         super().__init__()
+        self.path = ''
 
-        # LOAD UI
         loader = QtUiTools.QUiLoader()
+        
+        # MAINWINDOW WIDGET SETUP
         uifile = QtCore.QFile('forms/mainwindow.ui')
         uifile.open(QtCore.QFile.ReadOnly)
         self.ui = loader.load(uifile)
         uifile.close()
+        self.ui.setWindowIcon(QtGui.QIcon('img/icon.png'))
+        
+        self.index_table_ctrl = \
+            IndexTableCtrl(self.ui.table_root_index)
 
+        # NEW/CHANGE DIALOG SETUP
+        self.new_change_index = NewChangeIndex(self.index_table_ctrl)
+        
+        # CONNECT SIGNALS
+        self.ui.action_save_db.triggered.connect(self.save)
+        self.ui.action_save_as_db.triggered.connect(self.saveAs)
+        self.ui.action_open_db.triggered.connect(self.openFile)
 
-        self.index_table_ctrl = IndexTableCtrl(self.ui.__dict__['table_root_index'])
+        self.ui.action_new_entry.triggered.connect(
+            self.new_change_index.showCreate)
+        
+    def save(self):
+
+        if self.path == '':
+            self.saveAs()
+        
+        data = {}
+        data['.index_table_ctrl.data'] = self.index_table_ctrl.data
+
+        with open(self.path, 'wb') as f:
+            pickle.dump(data, f, 3)
+
+    def saveAs(self):
+
+        file_name = QtGui.QFileDialog.getSaveFileName(
+            self.ui, 
+            'Save Nelia file', 
+            os.path.expanduser('~/Documents/save.nelia'), 
+            'Nelia Files (*.nelia)')[0]
+
+        if file_name.rfind('.nelia') != len(file_name) - 6:
+            file_name += '.nelia'
+
+        self.path = file_name
+        self.save()
+
+    def openFile(self):
+    
+        self.path = QtGui.QFileDialog.getOpenFileName(
+            self.ui, 'Open Nelia file', 
+            os.path.expanduser('~/Documents'), 
+            'Nelia Files (*.nelia)')[0]
+
+        data = {}
+        with open(self.path, 'rb') as f:
+            data = pickle.load(f)
+
+        self.index_table_ctrl.reload_data(data['.index_table_ctrl.data'])
 
 
 # vim: set ts=4 sw=4 ai si expandtab:
