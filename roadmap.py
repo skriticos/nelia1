16e24d610dc93b3874b3a75dffb1aceb27176645
Sebastian Bartos <seth.kriticos@gmail.com>
2012-12-13 10:13:58 +0100

    roadmap fixes, addons
    - checkboxes work now
    - added created time to view
    - milestone selection changes now properly update table
    - adding feature touches project

--- roadmap.py
+++ roadmap.py
@@ -19,7 +19,7 @@ class NxRoadmap(QObject):
         self.widget.push_milestone.setMenu(self.milestone_menu)
 
         self.feature_headers = \
-            ['Name', 'Type', 'Target', 'Priority', 'Status', 'ID']
+            ['Name', 'Type', 'Target', 'Priority', 'Status', 'Created', 'ID']
 
         self.model = QStandardItemModel()
         self.table = self.widget.table
@@ -59,6 +59,11 @@ class NxRoadmap(QObject):
             d.radio_medium.setChecked(True)
             d.radio_feature.setChecked(True)
 
+            self.widget.check_feature.stateChanged.connect(self.reloadTables)
+            self.widget.check_issue.stateChanged.connect(self.reloadTables)
+            self.widget.check_open.stateChanged.connect(self.reloadTables)
+            self.widget.check_closed.stateChanged.connect(self.reloadTables)
+
     def onChangeVersionSelection(self, x, y, current_text):
 
         self.selected_x = x
@@ -77,36 +82,69 @@ class NxRoadmap(QObject):
         self.table.setColumnWidth(2, 80)
         self.table.setColumnWidth(3, 80)
         self.table.setColumnWidth(4, 80)
-        self.table.setColumnWidth(5, 80)
+        self.table.setColumnWidth(5, 160)
+        self.table.setColumnWidth(6, 80)
 
         pid = self.data.run['project'].getSelectedProject()
         pro = self.data.project[pid]
         x, y = pro['meta']['current_milestone']
         yy = self.selected_y
-        if self.selected_x == 0: yy == self.selected_y-1
-        fo = pro['milestone'][self.selected_x][y]['fo']
-        fc = pro['milestone'][self.selected_x][y]['fc']
+        if self.selected_x == 0: yy = self.selected_y-1
+        fo = pro['milestone'][self.selected_x][yy]['fo']
+        fc = pro['milestone'][self.selected_x][yy]['fc']
+        io = pro['milestone'][self.selected_x][yy]['io']
+        ic = pro['milestone'][self.selected_x][yy]['ic']
 
-        for key, value in fc.items():
-            self.model.insertRow(0, [
-                QStandardItem(value['name']),
-                QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
-                QStandardItem(str(value['priority'])),
-                QStandardItem(value['type']),
-                QStandardItem('No'),
-                QStandardItem(str(key))
-            ])
-            self.table.selectRow(0)
-        for key, value in fo.items():
-            self.model.insertRow(0, [
-                QStandardItem(value['name']),
-                QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
-                QStandardItem(str(value['priority'])),
-                QStandardItem(value['type']),
-                QStandardItem('No'),
-                QStandardItem(str(key))
-            ])
-            self.table.selectRow(0)
+        if self.widget.check_feature.isChecked():
+            if self.widget.check_closed.isChecked():
+                for key, value in fc.items():
+                    self.model.insertRow(0, [
+                        QStandardItem(value['name']),
+                        QStandardItem(value['ri_type']),
+                        QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
+                        QStandardItem(value['priority']),
+                        QStandardItem('Closed'),
+                        QStandardItem(datetime.datetime.fromtimestamp(value['created']).isoformat()),
+                        QStandardItem(str(key))
+                    ])
+                    self.table.selectRow(0)
+            if self.widget.check_open.isChecked():
+                for key, value in fo.items():
+                    self.model.insertRow(0, [
+                        QStandardItem(value['name']),
+                        QStandardItem(value['ri_type']),
+                        QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
+                        QStandardItem(value['priority']),
+                        QStandardItem('Open'),
+                        QStandardItem(datetime.datetime.fromtimestamp(value['created']).isoformat()),
+                        QStandardItem(str(key))
+                    ])
+                    self.table.selectRow(0)
+        if self.widget.check_issue.isChecked():
+            if self.widget.check_closed.isChecked():
+                for key, value in ic.items():
+                    self.model.insertRow(0, [
+                        QStandardItem(value['name']),
+                        QStandardItem(value['ri_type']),
+                        QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
+                        QStandardItem(value['priority']),
+                        QStandardItem('Closed'),
+                        QStandardItem(datetime.datetime.fromtimestamp(value['created']).isoformat()),
+                        QStandardItem(str(key))
+                    ])
+                    self.table.selectRow(0)
+            if self.widget.check_open.isChecked():
+                for key, value in io.items():
+                    self.model.insertRow(0, [
+                        QStandardItem(value['name']),
+                        QStandardItem(value['ri_type']),
+                        QStandardItem('{}.{}'.format(self.selected_x, self.selected_y)),
+                        QStandardItem(value['priority']),
+                        QStandardItem('Open'),
+                        QStandardItem(datetime.datetime.fromtimestamp(value['created']).isoformat()),
+                        QStandardItem(str(key))
+                    ])
+                    self.table.selectRow(0)
         self.table.setFocus()
         if self.table.currentIndex().row() == 0:
             self.widget.push_edit.setEnabled(True)
@@ -174,6 +212,7 @@ class NxRoadmap(QObject):
                 QStandardItem('{}.{}'.format(tx,ty)),
                 QStandardItem(prio),
                 QStandardItem('Open'),
+                QStandardItem(datetime.datetime.fromtimestamp(timestamp).isoformat()),
                 QStandardItem(str(self.data.project[pid]['meta']['last_roadmap_item']+1))
             ])
 
@@ -212,6 +251,8 @@ class NxRoadmap(QObject):
             self.widget.push_edit.setEnabled(True)
             self.widget.push_delete.setEnabled(True)
             self.widget.push_close.setEnabled(True)
+
+        self.data.run['project'].touchProject(timestamp)
 
 # vim: set ts=4 sw=4 ai si expandtab:
 
