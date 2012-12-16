0ee6601865fec61d7670bbb258c026aabc9acf1a
Sebastian Bartos <seth.kriticos@gmail.com>
2012-12-16 15:23:07 +0100

    Started adding milestone management to milestone.py

--- milestone.py
+++ milestone.py
@@ -2,21 +2,31 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 import time
 
+"""
+    Naming Conventions
+
+        pid: project id
+        major: major version
+        minor: minor version
+        itype: item type 'Feature'|'Issue'
+        status: item status 'Open'|'Closed'
+        icat: item category 'Core'|'Auxiliary'|'Security'|'Corrective'|'Architecture'|'Refactor'
+        name: name
+        priority: 'Low'|'Medium'|'High'
+        description: free form plain text
+
+    TODO
+
+        Integrate roadmap management (e.g. at adding, moving and deleting roadmap items)
+"""
+
 class NxMilestone:
 
-    def __init__(self, parent, data):
+    def __init__(self, data):
 
-        self.parent = parent
         self.data = data
 
     def versionToIndex(self, major, minor):
-
-        """
-            Helper.
-            Shorthand to get array address from milestone version number.
-            This has to be done, as the major version 0 branch starts with
-            minor version 1, and all others with 0.
-        """
 
         if major > 0:
             return major, minor
@@ -25,17 +35,30 @@ class NxMilestone:
 
     def indexToVersion(self, x, y):
 
-        if major > 0:
+        if x > 0:
             return x, y
         else:
             return x, y + 1
 
-    def getItemData(self, pid, itemId):
+    def addMilestone(self, pid, major, minor):
+
+        x, y = self.versionToIndex(major, minor)
+
+        if y == 0:
+            self.data.project[pid] ['milestone'].append(
+                [{'m': '{}.{}'.format(major, minor), 'fo': {}, 'fc': {}, 'io': {}, 'ic': {}}]
+            )
+        else:
+            self.data.project[pid] ['milestone'] [x].append(
+                {'m': '{}.{}'.format(major, minor), 'fo': {}, 'fc': {}, 'io': {}, 'ic': {}}
+            )
+
+    def getItemData(self, pid, item_id):
 
         p = self.data.project[pid]
-        ma, mi, fioc = p['ri_index'][item_id]
+        ma, mi, fioc = p['ri_index'] [item_id]
         x, y = self.versionToIndex(ma, mi)
-        item = p['milestone'][x][y][fioc]
+        item = p['milestone'] [x] [y] [fioc] [item_id]
 
         if fioc[0] == 'f':
             itype = 'Feature'
@@ -65,37 +88,24 @@ class NxMilestone:
         idat = self.getItemData(pid, itme_id)
         return idat[attr_name]
 
-    def setAttibute(self, pid, itme_id, attr_name, value):
+    def setAttibute(self, pid, item_id, attr_name, value):
 
-        idat = self.getItemData(pid, itme_id)
-        self.data.project[pid]['milestone'][idat['x']][idat['y']][idat['fioc']][item_id][attr_name] = value
+        idat = self.getItemData(pid, item_id)
+        self.data.project[pid] ['milestone'] [idat['x']] [idat['y']] [idat['fioc']] [item_id] [attr_name] = value
 
     def touchItem(self, pid, item_id):
 
-        self.setAttibute(self, pid, item_id, 'modified', int(time.time()))
+        self.setAttibute(pid, item_id, 'modified', int(time.time()))
 
     def addItem(self, pid, major, minor, itype, icat, name, priority, description, status='Open'):
 
-        """
-            p: project id
-            major: major version
-            minor: minor version
-            itype: item type 'Feature'|'Issue'
-            icat: item category 'Functional'|'Security'|'Convenience'
-            name: name
-            priority: 'Low'|'Medium'|'High'
-            description: free form plain text
-            status: item status 'Open'|'Closed'
-        """
-
-        item_id = self.data.project[pid]['meta']['last_roadmap_item'] + 1
+        item_id = self.data.project[pid] ['meta'] ['last_roadmap_item'] + 1
 
         new_item = {
             'name': name,
             'icat': icat,
             'priority': priority,
             'description': description,
-            'status': status,
             'created': int(time.time()),
         }
 
@@ -105,11 +115,27 @@ class NxMilestone:
         elif itype == 'Issue':
             fioc = 'io'
 
-        self.data.project[pid]['milestone'][x][y][fioc] = new_item
-        self.data.project[pid]['ri_index'][item_id] = (major, minor, fioc)
+        self.data.project[pid] ['milestone'] [x] [y] [fioc] [item_id] = new_item
+        self.data.project[pid] ['ri_index'] [item_id] = (major, minor, fioc)
 
+        self.data.project[pid] ['meta'] ['last_roadmap_item'] += 1
         self.touchItem(pid, item_id)
-        self.data.project[pid]['meta']['last_roadmap_item'] += 1
+
+        # if we add feature to an empty milestone
+        item_count = (
+                len(self.data.project[pid] ['milestone'] [x] [y] ['fo']) +
+                len(self.data.project[pid] ['milestone'] [x] [y] ['fc']) +
+                len(self.data.project[pid] ['milestone'] [x] [y] ['io']) +
+                len(self.data.project[pid] ['milestone'] [x] [y] ['ic'])
+        )
+
+        if item_count == 1:
+            # add milestones as required
+            if minor == 0:
+                self.addMilestone(pid, major, 1)
+                self.addMilestone(pid, major+1, 0)
+            else:
+                self.addMilestone(pid, major, minor+1)
 
     def editItem(self, pid, major, minor, item_id, itype, icat, name, priority, description):
 
@@ -137,23 +163,23 @@ class NxMilestone:
         else:
             if itype == 'Feature':
                 if not status:
-                    nfioc = 'f' + idat['fioc'][1]
+                    nfioc = 'f' + idat['fioc'] [1]
                 elif status == 'Open':
                     nfioc = 'fo'
                 elif status == 'Closed':
                     nfioc = 'fc'
             elif itype == 'Issue':
                 if not status:
-                    nfioc = 'i' + idat['fioc'][1]
+                    nfioc = 'i' + idat['fioc'] [1]
                 elif status == 'Open':
                     nfioc = 'io'
                 elif status == 'Closed':
                     nfioc = 'ic'
 
-        item = self.data.project[pid]['milestone'][idat['x']][idat['y']][idat['fioc']]
-        self.data.project[pid]['milestone'][nx][ny][nfioc][item_id] = item
-        del self.data.project[pid]['milestone'][idat['x']][idat['y']][idat['fioc']]
-        self.data.project[pid]['ri_index'][item_id] = (nx, ny, nfioc)
+        item = self.data.project[pid] ['milestone'] [idat['x']] [idat['y']] [idat['fioc']]
+        self.data.project[pid] ['milestone'] [nx] [ny] [nfioc] [item_id] = item
+        del self.data.project[pid] ['milestone'] [idat['x']] [idat['y']] [idat['fioc']]
+        self.data.project[pid] ['ri_index'] [item_id] = (nx, ny, nfioc)
         self.touchItem(pid, item_id)
 
     def closeItem(self, pid, item_id):
@@ -165,6 +191,52 @@ class NxMilestone:
     def deleteItem(self, pid, item_id):
 
         idat = self.getItemData(pid, item_id)
-        del self.data.project[pid]['milestone'][idat['x']][idat['y']][idat['fioc']][item_id]
-        del self.data.project[pid]['ri_index'][item_id]
+        del self.data.project[pid] ['milestone'] [idat['x']] [idat['y']] [idat['fioc']] [item_id]
+        del self.data.project[pid] ['ri_index'] [item_id]
+
+if __name__ == '__main__':
+
+    from datastore import NxDataStore
+    from pprint import pprint
+    data = NxDataStore(None)
+    control = NxMilestone(data)
+    for pid in range(1,3):
+        data.project[pid] = \
+            {  'meta': {'last_log': 0, 'last_roadmap_item': 0, 'current_milestone': (0,0)},
+               'log': {},
+               'milestone' : [
+                       [{'m': '0.1', 'fo': {}, 'fc': {}, 'io': {}, 'ic': {}}],
+                       [{'m': '1.0', 'fo': {}, 'fc': {}, 'io': {}, 'ic': {}}]
+                   ],
+               'ri_index' : {}
+            }
+    data.project[0] ['next_id'] = 3
+
+    assert control.versionToIndex(0, 1) == (0, 0)
+    assert control.versionToIndex(3, 3) == (3, 3)
+    assert control.indexToVersion(0, 0) == (0, 1)
+    assert control.indexToVersion(3, 3) == (3, 3)
+
+    pid = 1
+    major = 0
+    minor = 1
+    itype = 'Feature'
+    icat = 'Core'
+    name = 'Test Item 1'
+    priority = 'Medium'
+    description = 'Description Text 1'
+    control.addItem(pid, major, minor, itype, icat, name, priority, description, status='Open')
+    control.addItem(pid, major, minor, itype, icat, name, priority, description, status='Open')
+    pid = 1
+    major = 1
+    minor = 0
+    itype = 'Feature'
+    icat = 'Core'
+    name = 'Test Item 1'
+    priority = 'Medium'
+    description = 'Description Text 1'
+    control.addItem(pid, major, minor, itype, icat, name, priority, description, status='Open')
+
+    pprint (data.project)
+
 
