8608a9dc4b5a46c96834c2f7725b918921c588dc
Sebastian Bartos <seth.kriticos@gmail.com>
2012-12-19 16:15:13 +0100

    log: correction, adapted to new widget design

--- log.py
+++ log.py
@@ -9,35 +9,61 @@ class NxLog:
 
     def __init__(self, parent, datastore, widget):
 
+        # setup backbone
         self.parent = parent
         self.data   = datastore
         self.widget = widget
 
+        self.diag_new = self.parent.w_log_diag_new
+
+        # setup table
         self.table = widget.table_history
         self.model = QStandardItemModel()
         self.table_headers = \
                 ['ID', 'Created', 'Summary']
         self.model.setHorizontalHeaderLabels(self.table_headers)
         self.table.setModel(self.model)
+        self.selection_model = self.table.selectionModel()
 
+        # connect add roadmap callbacks
         self.widget.push_new_entry.clicked.connect(lambda: (
+            self.parent.w_log_diag_new.text_detail.clear(),
             self.parent.w_log_diag_new.line_summary.clear(),
             self.parent.w_log_diag_new.line_summary.setFocus(),
             self.parent.w_log_diag_new.show()))
-
         self.parent.w_log_diag_new.accepted.connect(self.onNewEntry)
+
+        # update text_detail on selection change
+        self.selection_model.selectionChanged.connect(self.onSelectionChange)
+
+    def getSelectedLogId(self):
+
+        return int(self.model.itemFromIndex(
+                self.model.index(self.table.currentIndex().row(), 0)).text())
+
+    def onSelectionChange(self):
+
+        if self.model.rowCount() > 0 and not self.init:
+            self.widget.text_detail.setEnabled(True)
+            self.widget.text_detail.setPlainText(
+                self.data.project[self.pid] ['log'] [self.getSelectedLogId()] ['detail']
+            )
 
     def onShowTab(self):
 
+        # retrive pid
         pid = self.data.run['project'].getSelectedProject()
 
+        # check if project selection changed
         if self.data.run['log_pid_last'] != pid:
 
+            # set data
+            self.pid = pid
             self.data.run['log_pid_last'] = pid
+            self.init = True # skip selection change callback
 
-            self.log = self.data.project[pid]['log']
+            # setup widget
             pname = self.data.run['project'].getSelectedProjectName()
-
             self.widget.line_project.setText(pname)
             self.parent.w_log_diag_new.line_project.setText(pname)
 
@@ -46,46 +72,54 @@ class NxLog:
             self.table.setColumnWidth(1, 160)
             self.table.setColumnWidth(2, 550)
 
+            # populate table
             for i in range(self.data.project[pid]['meta']['last_log']):
-                log = self.log[i+1]
+                log = self.data.project[pid]['log'][i+1]
                 self.model.insertRow(0, [
+                    QStandardItem(str(i+1)),
                     QStandardItem(datetime.datetime.fromtimestamp(log['created']).isoformat()),
-                    QStandardItem(log['summary']),
-                    QStandardItem(str(i+1)) ])
+                    QStandardItem(log['summary'])
+                ])
 
+            # setup state
+            self.table.sortByColumn(0, Qt.DescendingOrder)
+
+            self.init = False # re-enable selection change callback
             if self.data.project[pid]['meta']['last_log'] > 0:
                 self.table.selectRow(0)
-                self.table.sortByColumn(0, Qt.DescendingOrder)
-
-            self.table.setFocus()
+                self.table.setFocus()
+            else:
+                self.widget.text_detail.setPlainText('No log selected')
+                self.widget.text_detail.setEnabled(False)
+                self.widget.push_new_entry.setFocus()
 
     def onNewEntry(self):
 
-        pid = self.data.run['project'].getSelectedProject()
-        lid = self.data.project[pid]['meta']['last_log'] + 1
+        lid = self.data.project[self.pid]['meta']['last_log'] + 1
         timestamp = int(time.time())
-        d = self.parent.w_log_diag_new
         disptime = datetime.datetime.fromtimestamp(timestamp).isoformat()
 
-        self.data.project[pid]['log'][lid] = {
+        # populate data
+        self.data.project[self.pid]['log'][lid] = {
             'created': timestamp,
-            'summary': d.line_summary.text(),
-            'detail': d.text_detail.toPlainText()
+            'summary': self.diag_new.line_summary.text(),
+            'detail':  self.diag_new.text_detail.toPlainText()
         }
 
+        # update
+        self.data.run['project'].touchProject(timestamp)
+        self.data.project[self.pid]['meta']['last_log'] += 1
+
+        # populate data
         self.model.insertRow(
             0, [
+                QStandardItem(str(lid)),
                 QStandardItem(disptime),
-                QStandardItem(d.line_summary.text()),
-                QStandardItem(str(lid))
+                QStandardItem(self.diag_new.line_summary.text())
             ]
         )
 
+        # update state
         self.table.selectRow(0)
         self.table.setFocus()
-
-        self.data.run['project'].touchProject(timestamp)
-        self.data.project[pid]['meta']['last_log'] += 1
-
-# vim: set ts=4 sw=4 ai si expandtab:
 
