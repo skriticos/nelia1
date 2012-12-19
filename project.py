71d946493c73e2bb21c514bb5965fda3b604ae41
Sebastian Bartos <seth.kriticos@gmail.com>
2012-12-19 18:17:30 +0100

    project.py/ui: refactor: adjusted to new design.

--- project.py
+++ project.py
@@ -9,6 +9,7 @@ class NxProject:
 
     def __init__(self, parent, datastore, widget):
 
+        # setup backbone
         self.parent = parent
         self.data   = datastore
         self.widget = widget
@@ -33,7 +34,10 @@ class NxProject:
         self.data.run['w_project_diag_edit'].accepted.connect(self.onEditProject)
         self.widget.push_delete.clicked.connect(self.onDeleteProject)
 
-        self.widget.push_open.clicked.connect(lambda: (self.data.load() and self.reset()))
+        self.widget.push_open.clicked.connect(lambda: (
+            self.data.load(),
+            self.reset()
+        ))
         self.widget.push_save.clicked.connect(self.data.save)
 
         self.widget.push_help.clicked.connect(self.parent.w_project_diag_help.show)
@@ -47,11 +51,112 @@ class NxProject:
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(self.table_headers)
         self.table.setModel(self.model)
+        self.selection_model = self.table.selectionModel()
+
+        self.diag_new = self.parent.w_project_diag_new
+        self.diag_edit = self.parent.w_project_diag_edit
+
+        self.selection_model.selectionChanged.connect(self.onSelectionChanged)
+        self.widget.text_description.textChanged.connect(self.onDescriptionChange)
+
+    def touchProject(self, timestamp):
+
+        """
+            On project change (e.g. edit meta, add log or change roadmap),
+            this should be called with the timestamp. It will update the
+            last changed timestamp and update the project index display
+            and mark changes as true.
+        """
+        self.data.project[self.getSelectedProject()]['modified'] = timestamp
+        self.model.setItem(self.getActiveRow(), 8,
+            QStandardItem(datetime.datetime.fromtimestamp(timestamp).isoformat()))
+        self.data.run['changed'] = True
+
+    def getSelectedProject(self):
+        if not self.init:
+            if self.table.currentIndex().row() == -1:
+                return 0
+            return int(self.model.itemFromIndex(
+                    self.model.index(self.table.currentIndex().row(), 0)).text())
+
+
+    def getSelectedProjectName(self):
+        return self.model.itemFromIndex(
+                self.model.index(self.table.currentIndex().row(), 1)).text()
+
+    def getActiveRow(self):
+        return self.table.currentIndex().row()
+
+    def onSelectionChanged(self):
+
+        if not self.init:
+            pid = self.getSelectedProject()
+            if self.data.project[0]['next_id'] > 1 and pid != 0:
+                self.widget.text_description.setEnabled(True)
+                self.widget.text_description.setPlainText(
+                    self.data.project[pid]['description']
+                )
+
+    def onDescriptionChange(self):
+
+        self.data.project[self.getSelectedProject()]['description'] = \
+                self.widget.text_description.toPlainText()
+
+    def reloadTable(self):
+
+        self.init = True
+        self.model.clear()
+        self.model.setHorizontalHeaderLabels(self.table_headers)
+
+        for pid, project in self.data.project.items():
+            if pid == 0: continue
+            major, minor = self.data.project[pid]['meta']['current_milestone']
+            disptime1 = datetime.datetime.fromtimestamp(project['modified']).isoformat()
+            disptime2 = datetime.datetime.fromtimestamp(project['created']).isoformat()
+            self.model.insertRow(0, [
+                QStandardItem(str(pid)),
+                QStandardItem(project['name']),
+                QStandardItem(project['status']),
+                QStandardItem(project['ptype']),
+                QStandardItem('{}.{}'.format(major,minor)),
+                QStandardItem(project['category']),
+                QStandardItem(str(project['priority'])),
+                QStandardItem(str(project['challenge'])),
+                QStandardItem(disptime1),
+                QStandardItem(disptime2),
+            ])
+
+        self.table.sortByColumn(0, Qt.DescendingOrder)
+
+        self.init = False
+
+        if len(self.data.project) > 1:
+
+            self.table.selectRow(0)
+
+            self.parent.enableTabs()
+            self.widget.push_edit.setEnabled(True)
+            self.widget.push_delete.setEnabled(True)
+            self.widget.push_save.setEnabled(True)
+
+            self.table.setFocus()
+
+        else:
+
+            self.widget.text_description.clear()
+            self.widget.text_description.setEnabled(False)
+            self.parent.dissableTabs()
+            self.widget.push_edit.setEnabled(False)
+            self.widget.push_delete.setEnabled(False)
+            self.widget.push_save.setEnabled(False)
+            self.widget.push_new.setFocus()
 
     def onNewProject(self):
 
         timestamp = int(time.time())
         pid = self.data.project[0]['next_id']
+
+        # create new project entry
         # meta: general project data
         # log: log data
         # milestone: versions, features, issues
@@ -63,88 +168,53 @@ class NxProject:
                                             ],
                                         'ri_index' : {}
                                      }
-        d = self.data.run['w_project_diag_new']
 
-        p['name']        = name        = d.line_name.text()
-        p['category']    = category    = d.combo_category.currentText()
-        p['status']      = status      = d.combo_status.currentText()
-        p['ptype']       = ptype       = d.combo_ptype.currentText()
-        p['priority']    = priority    = d.spin_priority.value()
-        p['challenge']   = challenge   = d.spin_challenge.value()
-        p['description'] = description = d.text_description.toPlainText()
+        p['name']        = name        = self.diag_new.line_name.text()
+        p['category']    = category    = self.diag_new.combo_category.currentText()
+        p['status']      = status      = self.diag_new.combo_status.currentText()
+        p['ptype']       = ptype       = self.diag_new.combo_ptype.currentText()
+        p['priority']    = priority    = self.diag_new.spin_priority.value()
+        p['challenge']   = challenge   = self.diag_new.spin_challenge.value()
+        p['description'] = description = self.diag_new.text_description.toPlainText()
         p['created']     = created     = timestamp
-
-        self.model.insertRow(0, [
-            QStandardItem(name),
-            QStandardItem(ptype),
-            QStandardItem(status),
-            QStandardItem(category),
-            QStandardItem(str(priority)),
-            QStandardItem(str(challenge)),
-            QStandardItem('0.0'),
-            QStandardItem(datetime.datetime.fromtimestamp(timestamp).isoformat()),
-            QStandardItem(str(pid))
-        ])
+        p['modified']    = timestamp
 
         self.data.project[0]['next_id'] += 1
 
-        self.table.selectRow(0)
-        self.table.setFocus()
-        self.parent.enableTabs()
-
-        self.widget.push_edit.setEnabled(True)
-        self.widget.push_delete.setEnabled(True)
-        self.widget.push_save.setEnabled(True)
-
+        self.reloadTable()
         self.touchProject(timestamp)
 
     def showEditProject(self):
 
-        pid = self.getSelectedProject()
-        p = self.data.project[pid]
-        d = self.parent.w_project_diag_edit
+        p = self.data.project[self.getSelectedProject()]
 
-        d.line_name.setText(p['name'])
-        d.combo_ptype.setCurrentIndex(d.combo_ptype.findText(p['ptype']))
-        d.combo_status.setCurrentIndex(d.combo_status.findText(p['status']))
-        d.combo_category.setCurrentIndex(d.combo_category.findText(p['category']))
-        d.spin_priority.setValue(p['priority'])
-        d.spin_challenge.setValue(p['challenge'])
-        d.text_description.setPlainText(p['description'])
+        self.diag_edit.line_name.setText(p['name'])
+        self.diag_edit.combo_ptype.setCurrentIndex(self.diag_edit.combo_ptype.findText(p['ptype']))
+        self.diag_edit.combo_status.setCurrentIndex(self.diag_edit.combo_status.findText(p['status']))
+        self.diag_edit.combo_category.setCurrentIndex(self.diag_edit.combo_category.findText(p['category']))
+        self.diag_edit.spin_priority.setValue(p['priority'])
+        self.diag_edit.spin_challenge.setValue(p['challenge'])
+        self.diag_edit.text_description.setPlainText(p['description'])
 
-        d.show()
-        d.line_name.setFocus()
+        self.diag_edit.show()
+        self.diag_edit.line_name.setFocus()
 
     def onEditProject(self):
 
         pid = self.getSelectedProject()
         p = self.data.project[pid]
-        d = self.parent.w_project_diag_edit
         timestamp = int(time.time())
         disptime = datetime.datetime.fromtimestamp(timestamp).isoformat()
 
-        p['name']        = name        = d.line_name.text()
-        p['category']    = category    = d.combo_category.currentText()
-        p['status']      = status      = d.combo_status.currentText()
-        p['ptype']       = ptype       = d.combo_ptype.currentText()
-        p['priority']    = priority    = d.spin_priority.value()
-        p['challenge']   = challenge   = d.spin_challenge.value()
-        p['description'] = description = d.text_description.toPlainText()
+        p['name']        = self.diag_edit.line_name.text()
+        p['category']    = self.diag_edit.combo_category.currentText()
+        p['status']      = self.diag_edit.combo_status.currentText()
+        p['ptype']       = self.diag_edit.combo_ptype.currentText()
+        p['priority']    = self.diag_edit.spin_priority.value()
+        p['challenge']   = self.diag_edit.spin_challenge.value()
+        p['description'] = self.diag_edit.text_description.toPlainText()
 
-        x, y = p['meta']['current_milestone']
-        for index, value in (
-                            (0, name),
-                            (1, ptype),
-                            (2, status),
-                            (3, category),
-                            (4, priority),
-                            (5, challenge),
-                            (6, '{}.{}'.format(x, y)),
-                            (7, disptime),
-                            (8, str(pid))):
-            self.model.setData(
-                self.model.index(self.getActiveRow(), index), value)
-
+        self.reloadTable()
         self.touchProject(timestamp)
 
     def onDeleteProject(self):
@@ -161,80 +231,14 @@ class NxProject:
             return
 
         del self.data.project[pid]
-        self.model.removeRow(self.getActiveRow())
-
-        if self.model.rowCount() == 0:
-            self.parent.dissableTabs()
-        else:
-            self.table.selectRow(0)
+        self.reloadTable()
 
         # can't touch deleted project, direct changed update
         self.data.run['changed'] = True
 
-    def touchProject(self, timestamp):
-
-        """
-            On project change (e.g. edit meta, add log or change roadmap),
-            this should be called with the timestamp. It will update the
-            last changed timestamp and update the project index display
-            and mark changes as true.
-        """
-        self.data.project[self.getSelectedProject()]['changed'] = timestamp
-        self.model.setItem(self.getActiveRow(), 7,
-            QStandardItem(datetime.datetime.fromtimestamp(timestamp).isoformat()))
-        self.data.run['changed'] = True
-
-
-    def getSelectedProject(self):
-        if self.table.currentIndex().row() == -1:
-            return 0
-        return int(self.model.itemFromIndex(
-                self.model.index(self.table.currentIndex().row(), 8)).text())
-
-
-    def getSelectedProjectName(self):
-        return self.model.itemFromIndex(
-                self.model.index(self.table.currentIndex().row(), 0)).text()
-
-    def getActiveRow(self):
-        return self.table.currentIndex().row()
-
     def reset(self):
 
-        self.model.clear()
-        self.model.setHorizontalHeaderLabels(self.table_headers)
-
-        self.table.setColumnWidth(0, 200)
-        self.table.setColumnWidth(4, 50)
-        self.table.setColumnWidth(5, 50)
-        self.table.setColumnWidth(6, 80)
-        self.table.setColumnWidth(7, 160)
-        self.table.setColumnWidth(8, 80)
-
-        for pid,project in self.data.project.items():
-            if pid == 0: continue
-            x, y = self.data.project[pid]['meta']['current_milestone']
-            disptime = datetime.datetime.fromtimestamp(project['changed']).isoformat()
-            self.model.insertRow(0, [
-                QStandardItem(project['name']),
-                QStandardItem(project['ptype']),
-                QStandardItem(project['status']),
-                QStandardItem(project['category']),
-                QStandardItem(str(project['priority'])),
-                QStandardItem(str(project['challenge'])),
-                QStandardItem('{}.{}'.format(x,y)),
-                QStandardItem(disptime),
-                QStandardItem(str(pid))
-            ])
-
-        self.table.selectRow(0)
-        self.table.setFocus()
-
-        self.parent.enableTabs()
-        self.widget.push_edit.setEnabled(True)
-        self.widget.push_delete.setEnabled(True)
-        self.widget.push_save.setEnabled(True)
-
+        self.reloadTable()
         self.data.run['changed'] = False
 
 # vim: set ts=4 sw=4 ai si expandtab:
