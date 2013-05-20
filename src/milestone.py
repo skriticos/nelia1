01e997e524898f3ddd5833808e42fb8f2e55cdef
Sebastian Bartos <seth.kriticos@gmail.com>
2013-05-20 21:55:03 +0200

    milestone: completed testing

--- src/milestone.py
+++ src/milestone.py
@@ -93,102 +93,8 @@ def deleteMI(miid):
     dc.sp.m._(major)._(minor).idx.v.remove(miid)
     updateMilestoneTree()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-if __name__ == '__main__':
-    # initial data
-    dc.sp.nextmiid.v    = 1
-    dc.sp.curr.major.v  = 0
-    dc.sp.curr.minor.v  = 0
-    dc.sp.midx.v = {}
-    dc.sp.m._(0)._(1).description.v = ''
-    dc.sp.m._(0)._(1).idx.v = set()
-    dc.sp.m._(1)._(0).description.v = ''
-    dc.sp.m._(1)._(0).idx.v = set()
-    dc.sp.m.idx.v = {0, 1}
-    dc.sp.m._(0).idx.v = {1}
-    dc.sp.m._(1).idx.v = {0}
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\initial data')
-    from datacore import _dcdump
-    _dcdump(dc.sp, 'sp')
-    print('\ndry-run updateMilestoneTree (should not change anything)')
-    updateMilestoneTree()
-    assert (dc.sp.m.idx.v == {0, 1})
-    assert (dc.sp.m._(0).idx.v == {1})
-    assert (dc.sp.m._(1).idx.v == {0})
-    _dcdump(dc.sp, 'sp')
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nadding feature to 0.1')
-    print('this should create milestone 0.2')
-    print('dc.sp.m._0._2.*')
-    print("run addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')")
-    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
-    _dcdump(dc.sp, 'sp')
-    assert(dc.sp.mi._(1).name.v == 'name')
-    assert(dc.sp.midx.v[1] == (0, 1))
-    assert(dc.sp.m._(0).idx.v == {1, 2})
-    assert(dc.sp.m._(1).idx.v == {0})
-    assert(dc.sp.m._(0)._(1).idx.v == {1})
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nremoving previously created milestone item')
-    print('this should remove the 0.2 milestone and the milestone item data')
-    deleteMI(1)
-    _dcdump(dc.sp, 'sp')
-    assert (dc.sp.m.idx.v == {0, 1})
-    assert (dc.sp.m._(0).idx.v == {1})
-    assert (dc.sp.m._(1).idx.v == {0})
-    assert (len(dc.sp.midx.v) == 0)
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nadding item to major milestone 1.0')
-    print('this should add 1.1 and 2.0')
-    addMI(1, 0, 'type', 'cat', 'name', 0, 'desc')
-    _dcdump(dc.sp, 'sp')
-    assert(dc.sp.m._(2).idx.v == {0})
-    assert(dc.sp.m._(1).idx.v == {0, 1})
-    assert(dc.sp.mi._(2).name.v == 'name')
-    assert(dc.sp.midx.v[2] == (1, 0))
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\ndeleting the previously created item')
-    print('this should pretty much reset the state (except the next mi))')
-    deleteMI(2)
-    _dcdump(dc.sp, 'sp')
-    assert (dc.sp.m.idx.v == {0, 1})
-    assert (dc.sp.m._(0).idx.v == {1})
-    assert (dc.sp.m._(1).idx.v == {0})
-    assert (len(dc.sp.midx.v) == 0)
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nchanging item property')
-    print('the name property should change')
-    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
-    editMI(0, 1, 3, 'type', 'cat', 'new name', 0, 'desc')
-    _dcdump(dc.sp, 'sp')
-    assert(dc.sp.mi._(3).name.v == 'new name')
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nmove item')
-    print('new item 5 should be moved for 0.1 -> 0.2')
-    addMI(0, 2, 'type', 'cat', 'name', 0, 'desc') # 4
-    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc') # 5
-    editMI(0, 2, 5, 'type', 'cat', 'name', 0, 'desc')
-    _dcdump(dc.sp, 'sp')
-    assert(dc.sp.midx.v[5] == (0, 2))
-    assert(5 in dc.sp.m._(0)._(2).idx.v)
-    print('test OK')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    print('\nresetting')
+def reset():
+    print('resetting')
     dc.__dict__ = {}
     dc.v = None
     dc.sp.nextmiid.v    = 1
@@ -202,33 +108,127 @@ if __name__ == '__main__':
     dc.sp.m.idx.v = {0, 1}
     dc.sp.m._(0).idx.v = {1}
     dc.sp.m._(1).idx.v = {0}
-    _dcdump()
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == '__main__':
+    # initial data
+    dc.sp.nextmiid.v    = 1
+    dc.sp.curr.major.v  = 0
+    dc.sp.curr.minor.v  = 0
+    dc.sp.midx.v = {}
+    dc.sp.m._(0)._(1).description.v = ''
+    dc.sp.m._(0)._(1).idx.v = set()
+    dc.sp.m._(1)._(0).description.v = ''
+    dc.sp.m._(1)._(0).idx.v = set()
+    dc.sp.m.idx.v = {0, 1}
+    dc.sp.m._(0).idx.v = {1}
+    dc.sp.m._(1).idx.v = {0}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\initial data')
+    from datacore import _dcdump
+    print('\ndry-run updateMilestoneTree (should not change anything)')
+    updateMilestoneTree()
+    assert (dc.sp.m.idx.v == {0, 1})
+    assert (dc.sp.m._(0).idx.v == {1})
+    assert (dc.sp.m._(1).idx.v == {0})
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nadding feature to 0.1')
+    print('this should create milestone 0.2')
+    print('dc.sp.m._0._2.*')
+    print("run addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')")
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    assert(dc.sp.mi._(1).name.v == 'name')
+    assert(dc.sp.midx.v[1] == (0, 1))
+    assert(dc.sp.m._(0).idx.v == {1, 2})
+    assert(dc.sp.m._(1).idx.v == {0})
+    assert(dc.sp.m._(0)._(1).idx.v == {1})
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nremoving previously created milestone item')
+    print('this should remove the 0.2 milestone and the milestone item data')
+    deleteMI(1)
+    assert (dc.sp.m.idx.v == {0, 1})
+    assert (dc.sp.m._(0).idx.v == {1})
+    assert (dc.sp.m._(1).idx.v == {0})
+    assert (len(dc.sp.midx.v) == 0)
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nadding item to major milestone 1.0')
+    print('this should add 1.1 and 2.0')
+    addMI(1, 0, 'type', 'cat', 'name', 0, 'desc')
+    assert(dc.sp.m._(2).idx.v == {0})
+    assert(dc.sp.m._(1).idx.v == {0, 1})
+    assert(dc.sp.mi._(2).name.v == 'name')
+    assert(dc.sp.midx.v[2] == (1, 0))
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\ndeleting the previously created item')
+    print('this should pretty much reset the state (except the next mi))')
+    deleteMI(2)
+    assert (dc.sp.m.idx.v == {0, 1})
+    assert (dc.sp.m._(0).idx.v == {1})
+    assert (dc.sp.m._(1).idx.v == {0})
+    assert (len(dc.sp.midx.v) == 0)
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nchanging item property')
+    print('the name property should change')
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    editMI(0, 1, 3, 'type', 'cat', 'new name', 0, 'desc')
+    assert(dc.sp.mi._(3).name.v == 'new name')
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nmove item')
+    print('new item 5 should be moved for 0.1 -> 0.2')
+    addMI(0, 2, 'type', 'cat', 'name', 0, 'desc') # 4
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc') # 5
+    editMI(0, 2, 5, 'type', 'cat', 'name', 0, 'desc')
+    assert(dc.sp.midx.v[5] == (0, 2))
+    assert(5 in dc.sp.m._(0)._(2).idx.v)
+    print('test OK')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     print('\nmove item to empty branch')
-    print('~ adding to 0.1')
+    reset()
+    # we start off by adding two items to the 0.1 branch. then we move one of
+    # the items to the 0.2 branch. Then we expect one item to be in the first
+    # branch and one to be in the second. the key is that a new brnach (0.3) has
+    # to be created
     addMI(0, 1, 'type', 'cat', 'name', 0, 'desc') # 1
-    print('~ adding to 0.1')
     addMI(0, 1, 'type', 'cat', 'name', 0, 'desc') # 2
-    print('~ moving from 0.1 to 0.2 (should create 0.3)')
     editMI(0, 2, 1, 'type', 'cat', 'name', 0, 'desc')
-    _dcdump(dc.sp, 'sp')
     assert(dc.sp.midx.v[1] == (0, 2))
     assert(dc.sp.m._(0).idx.v == {1, 2, 3})
-    assert(2 in dc.sp.m._(0)._(2).idx.v)
+    assert(1 in dc.sp.m._(0)._(2).idx.v)
     print('test OK')
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # TODO: test move to empty major milestone
-    # TODO: test move away from previous to last last item
-    # FIXME: make sure that when last open item is moved away, milestone is closed (roadmap)
-    # FIXME: make sure that when only item can not be moved away (roadmap)
-
-
-
-
-
-
-
+    print('\nmove to major milestone')
+    reset()
+    # creating two items and moving one to a new milestone
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    editMI(1, 0, 1, 'type', 'cat', 'name', 0, 'desc')
+    assert(dc.sp.m._(0).idx.v == {1, 2})
+    assert(dc.sp.m._(1).idx.v == {0, 1})
+    assert(dc.sp.m._(2).idx.v == {0})
+    assert(dc.sp.midx.v[1] == (1, 0))
+    assert(dc.sp.midx.v[2] == (0, 1))
+    reset()
+    # creating only one item and moving it to a new milestone
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    editMI(1, 0, 1, 'type', 'cat', 'name', 0, 'desc')
+    assert(dc.sp.m._(0).idx.v == {1})
+    assert(dc.sp.m._(1).idx.v == {0, 1})
+    assert(dc.sp.m._(2).idx.v == {0})
+    assert(dc.sp.midx.v[1] == (1, 0))
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\ntest move away from previous to last item')
+    reset()
+    addMI(0, 1, 'type', 'cat', 'name', 0, 'desc')
+    addMI(0, 2, 'type', 'cat', 'name', 0, 'desc')
+    deleteMI(2)
+    assert(dc.sp.m._(0).idx.v == {1, 2})
+    print('test OK')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
