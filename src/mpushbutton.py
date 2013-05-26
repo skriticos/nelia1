6e56926a52226a9b7de227356bb7f220c96ba467
Sebastian Bartos <seth.kriticos@gmail.com>
2013-05-26 17:16:40 +0200

    mpushbutton: cleanup

--- src/mpushbutton.py
+++ src/mpushbutton.py
@@ -25,7 +25,6 @@ class MPushButton(QPushButton):
             mfo = mfc = mio = mic = 0 # reset major version feature counters
             for m in dc.sp.m._(n).idx.v: # m == minor versions
                 action = QAction(self)
-                # compute minor version feature / issue count
                 fo = fc = io = ic = 0
                 for miid in dc.sp.m._(n)._(m).idx.v:
                     if dc.sp.mi._(miid).itype.v == 'Feature':
@@ -38,28 +37,23 @@ class MPushButton(QPushButton):
                             io += 1
                         else:
                             ic += 1
-                # add to major version feature / issue count
                 mfo += fo
                 mfc += fc
                 mio += io
                 mic += ic
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                # we are on second level iteration n.m, x.y is current version
-                # if current version = 0.0 (no milestone reached)
                 if x == 0 and y == 0:
                     if n == 0:
                         Δm = m
                     if n > 0:
                         Δm = sum(len(dc.sp.m._(s).idx.v) for s in range(n)) \
                             + m + 1
-                # current version = 0.y, y>0
                 if x == 0 and y > 0:
                     if n == 0:
                         Δm = m - y
                     if n > 0:
                         Δm = sum(len(dc.sp.m._(s).idx.v) for s in range(1,n)) \
                             + m + len(dc.sp.m._(0).idx.v) - y + 1
-                # current version = 1.y, y>=0
                 if x == 1:
                     if n == 0:
                         Δm = -1 * (y + (len(dc.sp.m._(0).idx.v - m))) - 1
@@ -68,7 +62,6 @@ class MPushButton(QPushButton):
                     if n > x:
                         Δm = sum(len(dc.sp.m._(s).idx.v) for s in range(x+1,n))\
                             + m + len(dc.sp.m._(x).idx.v) - y
-                # current version = x.y, x>1
                 if x > 1:
                     if n < x:
                         Δm = -1 * ((len(dc.sp.m._(n).idx.v) - m)
@@ -82,7 +75,6 @@ class MPushButton(QPushButton):
                         Δm = sum(len(dc.sp.m._(s).idx.v) for s in range(x+1,n))\
                             + m + len(dc.sp.m._(x).idx.v) - y
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                # compute completion symbol and sign
                 oo = False
                 if Δm > 1:
                     sign = '+'
@@ -102,12 +94,7 @@ class MPushButton(QPushButton):
                     icon = '◆'
                 if open_only and not oo:
                     continue
-                # Compute major and minor version combined delta.
-                # Notice how this has nothing to do with floating point.
-                # Instead it's two deltas, major, and combined minor.
                 Δnm = '{}{},{}'.format(sign, abs(Δn), abs(Δm))
-                # compute minor version label,
-                # set it to action and add action to major_menu
                 label = '{}   v{}.{}   {}   f:{}/{}   i:{}/{}'.format(
                     icon,n,m,Δnm,fc,fo+fc,ic,io+ic)
                 if sel_x == n and sel_y == m and open_only and Δm > 0:
@@ -123,7 +110,6 @@ class MPushButton(QPushButton):
                 action.triggered.connect(self.selectionChanged)
                 major_menu.addAction(action)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # compute major version icon
             oo = False
             if Δn > 0:
                 icon = '◇'
@@ -136,7 +122,6 @@ class MPushButton(QPushButton):
             if open_only and not oo:
                 major_menu.close()
                 continue
-            # set major version menu label and add it to the root menu
             major_menu.setTitle('{}   v{}.x   f:{}/{}   i:{}/{}'.format(
                 icon, n, mfc, mfo+mfc, mic, mio+mic))
             self.root_menu.addMenu(major_menu)
@@ -150,7 +135,6 @@ class MPushButton(QPushButton):
         self.current_text = self.sender().text()
         if old_text == self.current_text: return
         self.setText(self.current_text)
-        # execute external callback, e.g. for roadmap table widget update
         if self.change_callback:
             x, y = self.getVersion()
             self.change_callback(x, y)
