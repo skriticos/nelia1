f1c05150a77091d092f9892777055d731d867d1d
Sebastian Bartos <seth.kriticos@gmail.com>
2013-06-28 12:42:20 +0200

    milestonecontrol: rename, add feature/issue diff, change

--- standalone/milestonecontrol/run.py
+++ standalone/milestonecontrol/run.py
@@ -6,54 +6,56 @@ from PySide.QtGui import *
 from PySide import QtUiTools
 import sys
 from datacore import dc, _dcdump
-from milestonectrl import *
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# UI INDEX
-# dc.ui.v.pushAddMi
-# dc.ui.v.pushCloseMi
-# dc.ui.v.pushDeleteMi
-# dc.ui.v.pushRun
-# dc.ui.v.testMPushButton
-# dc.ui.v.pushMoveMi
-# dc.ui.v.pushTargetMPB
-# dc.ui.v.textResult
+from milestonecontrol import *
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app = QApplication(sys.argv)
 def log(msg):
     print(msg)
-    dc.ui.v.textResult.appendPlainText(msg)
-def onAddMi():
-    log('adding milestone item')
-def onDeleteMi():
-    log('deleting milestone item')
-def onCloseMi():
-    log('closing milestone item')
-def onMoveMi():
-    log('moving milestone item')
-def onRun():
-    log('running test sequence')
+    dc.ui.v.xx_debug.appendPlainText(msg)
+def on_add_feature():
+    log('adding feature')
+def on_add_issue():
+    log('adding issue')
+def on_close_feature():
+    log('closing feature')
+def on_close_issue():
+    log('closing issue')
+def on_delete_feature():
+    log('deleting feature')
+def on_delete_issue():
+    log('deleting issue')
+def on_move_feature():
+    log('moving feature')
+def on_move_issue():
+    log('moving issue')
+def on_run_sequence():
+    log('running sequence')
 def closeMinorMilestone():
     log('closing minor milestone')
 def closeMajorMilestone():
     log('closing major milestone')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 loader = QtUiTools.QUiLoader()
-f = QFile('milestonectrl/test.ui')
+f = QFile('milestonecontrol/test.ui')
 f.open(QFile.ReadOnly)
 ui = dc.ui.v = loader.load(f)
 f.close()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ui.pushAddMi.clicked.connect(onAddMi)
-ui.pushCloseMi.clicked.connect(onCloseMi)
-ui.pushDeleteMi.clicked.connect(onDeleteMi)
-ui.pushRun.clicked.connect(onRun)
-ui.pushMoveMi.clicked.connect(onMoveMi)
+dc.ui.v.xx_add_feature	 .clicked.connect   (on_add_feature)
+dc.ui.v.xx_add_issue	 .clicked.connect   (on_add_issue)
+dc.ui.v.xx_close_feature .clicked.connect   (on_close_feature)
+dc.ui.v.xx_close_issue	 .clicked.connect   (on_close_issue)
+dc.ui.v.xx_delete_feature.clicked.connect   (on_delete_feature)
+dc.ui.v.xx_delete_issue	 .clicked.connect   (on_delete_issue)
+dc.ui.v.xx_move_feature	 .clicked.connect   (on_move_feature)
+dc.ui.v.xx_move_issue	 .clicked.connect   (on_move_issue)
+dc.ui.v.xx_run_sequence	 .clicked.connect   (on_run_sequence)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # TODO: load MPBs
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if sys.argv[1] == '--auto':
     print('running automatic mode')
-    onRun()
+    on_run_sequence()
 if sys.argv[1] == '--gui':
     print('running gui mode')
     dc.ui.v.show()
