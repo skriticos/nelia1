473b3312b47bf0572814a39933298a718ebdd561
Sebastian Bartos <seth.kriticos@gmail.com>
2013-07-22 14:34:38 +0200

    extended test body
    - added mainloop to properly trigger shutdown event handling
    - added some info messages

--- src/run_test.py
+++ src/run_test.py
@@ -6,20 +6,33 @@ import sys, PySide, mainwindow
 from datacore import *
 from datacore import _dcNode
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-app = PySide.QtGui.QApplication(sys.argv)
-mainwindow.MainWindow(sys.argv, app)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def logTestResult(num, name, status):
     log('***** TEST CASE #{}, {} RESULT: {} *****'.format(
         num, name, status))
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+log('***** STARTING UP APPLICATION *****')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app = PySide.QtGui.QApplication(sys.argv)
+mainwindow.MainWindow(sys.argv, app)
+dc.ui.main.v.hide()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dct = _dcNode('dct') # test data core
 dct.case.v = 0
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 logTestResult(1, 'CREATE PROJECT', 'SUCCESS')
 
 # create a new project (programatically)
 # create a new project (gui)
 # ...
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+log('***** TEST RUN MAIN LOOP AND SHUT DOWN *****')
+def quitApp():
+    dc.ui.main.v.close()
+timer = PySide.QtCore.QTimer()
+timer.start(100)
+timer.timeout.connect(quitApp)
+app.exec_()
+log('***** BYE BYE *****')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
