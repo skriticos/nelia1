5640befb0aa5bcf5ba38a4348f24b41479f4c544
Sebastian Bartos <seth.kriticos@gmail.com>
2013-08-18 17:47:26 +0200

    progress commit
    * delete log entry
    * some states and callback state corrections
    * other stuff and cleanup

--- src/log.py
+++ src/log.py
@@ -22,13 +22,14 @@ states.startup = {
     'lbl_log_created':  {'clear': True},
     'lbl_log_modified': {'clear': True},
     'btn_log_delete':   {'enabled': False},
-    'text_log_message': {'enabled': False},
-    'line_log_summary': {'enabled': False}
+    'text_log_message': {'clear': True, 'enabled': False},
+    'line_log_summary': {'clear': True, 'enabled': False}
 }
 
 states.selected = {
     'text_log_message': {'clear': True, 'enabled': True},
-    'line_log_summary': {'clear': True, 'enabled': True}
+    'line_log_summary': {'clear': True, 'enabled': True},
+    'btn_log_delete' :  {'enabled': True}
 }
 
 states.description_normal = {
@@ -55,7 +56,6 @@ def enableSelectionCallback():
 @logger('(log) disableSelectionCallback()')
 def disableSelectionCallback():
 
-    return
     m = dc.x.log.selection_model.v
     m.selectionChanged.disconnect(onSelectionChanged)
 
@@ -76,6 +76,7 @@ def enableAllCallbacks():
 
     w = dc.ui.log.v
     w.btn_log_new               .clicked.connect(dc.m.log.v.onNewLogClicked)
+    w.btn_log_delete            .clicked.connect(dc.m.log.v.onDeleteLogClicked)
 
     # navi callbacks
     w = dc.ui.log.v
@@ -262,10 +263,13 @@ class loglist:
             lid   = int(dc.x.log.model.v.data(index))
             dc.x.log.slid.v = lid
 
+            disableEditCallbacks()
+            applyStates(states.selected, dc.ui.log.v)
+            enableEditCallbacks()
+
             s, r = QItemSelectionModel.Select, QItemSelectionModel.Rows
             dc.x.log.selection_model.v.setCurrentIndex(index, s|r)
             selection = dc.x.log.view.v.selectionModel().selection()
-            applyStates(states.selected, dc.ui.log.v)
 
             return
 
@@ -277,10 +281,14 @@ class loglist:
 
             # if we have a match, select it and abort
             if lid == dc.x.log.slid.v:
+
+                disableEditCallbacks()
+                applyStates(states.selected, dc.ui.log.v)
+                enableEditCallbacks()
+
                 s, r = QItemSelectionModel.Select, QItemSelectionModel.Rows
                 dc.x.log.selection_model.v.setCurrentIndex(index, s|r)
                 selection = dc.x.log.view.v.selectionModel().selection()
-                applyStates(states.selected, dc.ui.log.v)
                 break
 
 
@@ -340,106 +348,30 @@ class NxLog:
         dc.sp.log._(lid).description.v = ''
 
         loglist.reloadTable()
+        dc.m.project.v.touchProject()
         dc.ui.log.v.line_log_summary.setFocus()
 
-'''
+    @logger('NxLog.onDeleteLogClicked(self)', 'self')
+    def onDeleteLogClicked(self):
 
+        lid = dc.x.log.slid.v
 
-        self.view = dc.ui.log.v.table_history
-        self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(headers)
-        self.view.setModel(self.model)
-        self.selection_model = self.view.selectionModel()
-        self.horizontal_header = self.view.horizontalHeader()
-        self.view.setAlternatingRowColors(True)
-        dc.ui.log.v.push_new_entry.clicked.connect(self.onNewEntryClicked)
-        dc.ui.log_diag_new.v.accepted.connect(self.onNewSubmit)
-        self.selection_model.selectionChanged.connect(self.onSelectionChange)
-        if dc.x.config.loaded.v:
-            self.loadLayout()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.onNewEntryClicked(self)', 'self')
-    def onNewEntryClicked(self):
-        dc.ui.log_diag_new.v.text_detail.clear()
-        dc.ui.log_diag_new.v.line_summary.clear()
-        dc.ui.log_diag_new.v.show()
-        dc.ui.log_diag_new.v.line_summary.setFocus()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.onSelectionChange(self, item_selection, previous)',
-            'self', 'item_selection', 'previous')
-    def onSelectionChange(self, item_selection, previous):
-        indexes = item_selection.indexes()
-        if not indexes:
-            dc.ui.log.v.text_detail.setEnabled(False)
-            dc.ui.log.v.text_detail.setPlainText('No log selected')
+        # are you sure? dialog
+        t = 'Delete log?'
+        q = 'Sure you want to delete log {}: {}?'\
+                .format(str(lid), dc.sp.log._(lid).summary.v)
+        yes, no = QMessageBox.Yes, QMessageBox.No
+        response = QMessageBox.question(dc.ui.project.v, t, q, yes|no)
+        if response == QMessageBox.StandardButton.No:
             return
-        row = indexes[0].row()
-        index = self.model.index(row, 0)
-        slogid = int(self.model.itemFromIndex(index).text())
-        dc.ui.log.v.text_detail.setEnabled(True)
-        dc.ui.log.v.text_detail.setPlainText(dc.sp.log._(slogid).detail.v)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.saveLayout(self)', 'self')
-    def saveLayout(self):
-        dc.c.log.header.width.v = list()
-        for i in range(self.model.columnCount()):
-            dc.c.log.header.width.v.append(self.view.columnWidth(i))
-        column = self.horizontal_header.sortIndicatorSection()
-        order  = self.horizontal_header.sortIndicatorOrder().__repr__()
-        dc.c.log.sort.column.v = column
-        dc.c.log.sort.order.v  = order
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.loadLayout(self)', 'self')
-    def loadLayout(self):
-        for i,v in enumerate(dc.c.log.header.width.v):
-            self.view.setColumnWidth(i, v)
-        if dc.c.log.sort.column.v:
-            self.horizontal_header.setSortIndicator(
-                    dc.c.log.sort.column.v, convert(dc.c.log.sort.order.v))
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.reloadTable(self)', 'self')
-    def reloadTable(self):
-        self.saveLayout()
-        self.selection_model.clear()
-        self.model.clear()
-        self.model.setHorizontalHeaderLabels(headers)
-        for lid in range(1, dc.sp.nextlid.v):
-            self.model.insertRow(0, [
-                QStandardItem(str(lid).zfill(4)),
-                QStandardItem(convert(dc.sp.log._(lid).created.v)),
-                QStandardItem(dc.sp.log._(lid).summary.v) ])
-        for i in range(self.model.rowCount()):
-            index = self.model.index(i, 0)
-            lid = int(self.model.itemFromIndex(index).text())
-            if lid == self.slogid:
-                self.selection_model.select(index,
-                    QItemSelectionModel.Select|QItemSelectionModel.Rows)
-                break
-        self.loadLayout()
-        if dc.sp.nextlid.v > 1: self.view.setFocus()
-        else:                   dc.ui.log.v.push_new_entry.setFocus()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('NxLog.onShowTab(self)', 'self')
-    def onShowTab(self):
-        # check if project selection changed
-        if dc.r.log.pid.last.v != dc.spid.v:
-            dc.r.log.pid.last.v = dc.spid.v
-            dc.ui.log.v.line_project.setText(dc.sp.name.v)
-            dc.ui.log_diag_new.v.line_project.setText(dc.sp.name.v)
-            self.slogid = dc.sp.nextlid.v
-            self.reloadTable()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    @logger('onNewSubmit(self)', 'self')
-    def onNewSubmit(self):
-        lid = self.slogid = dc.sp.nextlid.v
-        dc.sp.log._(lid).created.v = int(time.time())
-        dc.sp.log._(lid).summary.v = dc.ui.log_diag_new.v.line_summary.text()
-        detail = dc.ui.log_diag_new.v.text_detail.toPlainText()
-        dc.sp.log._(lid).detail.v = detail
-        dc.sp.nextlid.v += 1
-        dc.m.project.v.touchProject()
-        self.reloadTable()
-        self.view.setFocus()
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
 
+        # remove log
+        dc.sp.log.index.v.remove(lid)
+        del dc.sp.log.__dict__['_{}'.format(lid)]
+        dc.x.log.slid.v = 0
+
+        # state
+        loglist.reloadTable()
+        dc.m.project.v.touchProject()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
