c8447ae5c4542e3451c95c697b38e49e892d2fda
Sebastian Bartos <seth.kriticos@gmail.com>
2013-10-23 22:08:50 +0200

    added mime type and file association

--- src/run.py
+++ src/run.py
@@ -123,15 +123,13 @@ if __name__ == '__main__':
             pa.append(s)
         p = ' '.join(pa)
         p = os.path.expanduser(p)
-        print(p)
         if os.path.exists(p):
 
             dcload(p)
             dc.spid.v = dc.s.spid.v
             dc.sp = dc.s._(dc.spid.v)
+            dc.states.project.selected.v = True
             dc.states.project.changed.v = False
-            dc.states.project.focustable.v = True
-            dc.states.project.newload.v = True
             dc.m.project.projectlist.v.reloadTable()
 
     # this interrupt is required to make system signal handling working
